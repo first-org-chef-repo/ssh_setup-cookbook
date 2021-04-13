7e5df6da7046b60a708b4055bbac3ae73ee62b94
r-goto <https://gl.creationline.com/r-goto>
2021-04-13 22:56:01 +0900

    insecure

--- recipes/setup_ssh_config.rb
+++ recipes/setup_ssh_config.rb
@@ -4,22 +4,22 @@
 #
 # Copyright:: 2021, The Authors, All Rights Reserved.
 
-cookbook_file '/etc/ssh/sshd_config' do
-  source 'sshd_config'
-  owner 'root'
-  group 'root'
-  mode '0600'
-  action :create
-  notifies :restart, 'service[sshd]'
-end
-
 # cookbook_file '/etc/ssh/sshd_config' do
-#   source 'insecure_config'
+#   source 'sshd_config'
 #   owner 'root'
 #   group 'root'
-#   mode '0755'
+#   mode '0600'
 #   action :create
+#   notifies :restart, 'service[sshd]'
 # end
+
+cookbook_file '/etc/ssh/sshd_config' do
+  source 'insecure_config'
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
 
 service 'sshd' do
   action :nothing
